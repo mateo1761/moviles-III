a39d5424fb9d285d368c1796a9e4d0d20e9e1277
mateo1761 <308@academicos.cesde.edu.co>
2022-03-29 21:02:29 -0500

    Se agrega bloc en la carpeta src

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -3,7 +3,7 @@ import logo from "../../public/logo192.png"
 
 export default function Menu (){
     return(
-        <nav className="w3-sidebar w3-collapse w3-white w3-animate-left" style={"z-index",3,"width","300px"} id="mySidebar"><br/>
+        <nav className="w3-sidebar w3-collapse w3-white w3-animate-left" style={{"z-index":3,"width":"300px"}} id="mySidebar"><br/>
             <div className="w3-container">
                 <a href="#" onclick="w3_close()" className="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
                 <i class="fa fa-remove"></i>
