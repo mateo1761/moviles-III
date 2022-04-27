802b9b9742b99e2b0db2a82e7496194a1d038525
mateo1761 <jamesherrera364@gmail.com>
2022-04-26 20:46:18 -0500

    Se consumes api

--- my-app/src/componentes/contenedor/articulos.js
+++ my-app/src/componentes/contenedor/articulos.js
@@ -1,13 +1,14 @@
 import React from "react";
 import img1 from "./../../imagenes/imagen3.jpg";
-export default function Articulos({Articulos}) {
-    console.log(Articulos);
+export default function Articulos({titulo, fecha, descripcion, imagen}) {
+    console.log(titulo, fecha, descripcion, imagen);
     return(
         <div className="w3-third w3-container w3-margin-bottom">
-            <img src={Articulos.img} alt="Norway" style={{width:"100%"}} className="w3-hover-opacity"/>
+            <img src={imagen} alt="Norway" style={{width:"100%"}} className="w3-hover-opacity"/>
             <div className="w3-container w3-white">
-                <p><b>{Articulos.titulo}</b></p>
-                <p>{Articulos.parrafo}</p>
+                <h2>{titulo}</h2>
+                <p><b>{fecha}</b></p>
+                <p>{descripcion}</p>
             </div>
         </div>
     );
--- my-app/src/componentes/contenedor/contenedor.js
+++ my-app/src/componentes/contenedor/contenedor.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Articulos from "./articulos";
-import {datosArticulos} from "./info_articulos"
+import "./estilos-contenedor.css"
 
 export default function Contenedor() {
+    
     //console.log(datosArticulos)
+
+    const[post, setNoticias] = useState([]);
+    //funcion asincrona para obtener los datos
+    const obtenerDatos= async ()=>{
+        let url = 'https://newsapi.org/v2/top-headlines?country=co&apiKey=bab9a417ee04471aa0fc006a9d94750b'
+        const respuesta =await fetch(url);
+        const datos = await respuesta.json();
+        console.log(datos.articles);
+        return setNoticias(datos.articles);
+    }
+    useEffect(()=>{
+        obtenerDatos();
+    },[])
+    
+
     return(
         <div className="w3-row-padding">
             {
-                datosArticulos.map((art)=>{
-                    return <Articulos key={art.id} Articulos={art}/>
+                post.map((art, index )=>{
+                    return <Articulos 
+                    key={index }
+                    titulo={art.title}
+                    fecha={art.fecha}
+                    descripcion={art.description}
+                    imagen={art.urlToImage}/>
                 })
             }
+            <div className="paginacion">
+                <button className="atras">Cargar Menos</button>
+                <span> 1 </span>
+                <button className="adelante">Cargar Más</button>
+            </div>
         </div>
     );
 }
